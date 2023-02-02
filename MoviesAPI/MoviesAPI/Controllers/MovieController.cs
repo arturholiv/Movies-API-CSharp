177a4efd5314aa90e9e4f3d4d29c78349f224d3d
Artur H. de Oliveira <artur.henrique45@hotmail.com>
2023-02-02 18:21:57 -0300

    feat: creates post in contrroller

---
+++ MoviesAPI/MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Models;
+  
+namespace MoviesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+
+        private static List<Movie> movies = new List<Movie>();
+
+        [HttpPost]
+        public void AddMovie([FromBody] Movie movie)
+        {
+            movies.Add(movie);
+            Console.WriteLine(movie.Title);
+            Console.WriteLine(movie.Gender);
+
+        }
+    }
+}
---
+++ MoviesAPI/MoviesAPI/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Models;
+
+public class Movie
+{
+    [Required(ErrorMessage = "The Movie title is required")]
+    [MaxLength(120)]
+    public string Title { get; set; }
+
+    [Required(ErrorMessage = "The Movie gender is required")]
+    [MaxLength(50, ErrorMessage = "The Movie gender can not be higher than 50" )]
+    public string Gender { get; set; }
+
+    [Required]
+    [Range(70, 600, ErrorMessage = "The Movie duration must be between 60 and 600 minutes")]
+    public int Duration { get; set; }
+}
